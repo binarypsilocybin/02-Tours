c81be71f1b7a6e191768a5f45548d1b6b308b8cf
binarypsilocybin <rogeriosvaldo@gmail.com>
2022-05-29 16:54:23 +0100

    1 file modified

--- src/App.js
+++ src/App.js
@@ -5,8 +5,24 @@ import Tours from './Tours';
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project';
 function App() {
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchTours();
+  }, []);
   if (loading) {
     return (
       <main>
