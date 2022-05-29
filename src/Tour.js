f851ad07ffd91bd28f02de52dd4a009c91ecae0f
binarypsilocybin <rogeriosvaldo@gmail.com>
2022-05-29 17:01:08 +0100

    3 files modified

--- src/Tour.js
+++ src/Tour.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+const Tour = ({ tour }) => {
+  const { id, name, info, image, price } = tour;
+  return <h2>{tour.name}</h2>;
 };
 
 export default Tour;
